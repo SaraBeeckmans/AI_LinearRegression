8c77c5a474c73660d3d8331b9a90dc16a1e5f80b
Sara Beeckmans <sara@sarabeeckmans.be>
2019-12-28 16:47:29 +0100

    working train

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,6 +4,17 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+import math
+
+
+
+def caluclateSum(col, sum):
+    for number in col:
+        sum = sum + number
+
+    return sum
+
+    #print(head +"= " + str(sum))
 
 
 pd.plotting.register_matplotlib_converters()
@@ -19,14 +30,49 @@ plt.ylabel('Y')
 sns.scatterplot(x=data['X'], y=data['Y'])
 sns.regplot(x=data['X'], y=data['Y'])
 
-X = data['X'].values.reshape(-1,1)
-Y = data['Y'].values.reshape(-1,1)
+X = data['X'].values.reshape(-1, 1)
+Y = data['Y'].values.reshape(-1, 1)
 
 regressor = LinearRegression()
 regressor.fit(X, Y)
 
-#To retrieve the intercept:
+# To retrieve the intercept:
 print(regressor.intercept_)
-#For retrieving the slope:
+# For retrieving the slope:
 print(regressor.coef_)
 plt.show(block=True)
+
+Xmean = data["X"].mean()
+Ymean = data["Y"].mean()
+print("Gemiddelde X = " + str(Xmean))
+print("Gemiddelde Y = " + str(Ymean))
+
+
+data["X-Xmean"] = data["X"]-Xmean
+data["Y-Ymean"] = data["Y"]-Ymean
+data["X-Xmean x Y-Ymean"] = data["X-Xmean"] * data["Y-Ymean"]
+data["(X-Xmean)^2"] = data["X-Xmean"] * data["X-Xmean"]
+data["(Y-Ymean)^2"] = data["Y-Ymean"] * data["Y-Ymean"]
+
+
+
+print(data.head())
+print()
+
+sum = 0
+sumfirst = caluclateSum(data["X-Xmean x Y-Ymean"], sum)
+sumsec = caluclateSum(data["(X-Xmean)^2"], sum)
+sumthird = caluclateSum(data["(Y-Ymean)^2"], sum)
+
+print("X-Xmean x Y-Ymean = " + str(sumfirst))
+print("(X-Xmean)^2 = " +str(sumsec))
+print("(Y-Ymean)^2 = " + str(sumthird))
+
+
+print()
+print("Calculate R")
+r = (sumfirst)/math.sqrt((sumsec) * (sumthird))
+print(r)
+
+
+
